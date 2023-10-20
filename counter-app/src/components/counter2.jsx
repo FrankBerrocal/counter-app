fd13482253bf61ee3fec7ca47a6cbcc0e0513553
FrankBerrocal <f.berrocalazofeif887@mybvc.ca>
2023-10-20 15:35:18 -0700

    Attributes, lists , rendering classes

--- counter-app/src/components/counter2.jsx
+++ counter-app/src/components/counter2.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';  //type imrc
 
 //in jsx we dont have loops, we have maps
-class Counter extends Component {  //type cc
+class Counter2 extends Component {  //type cc
     state ={ 
         count: 0,
         tags: ['tag1', 'tag2', 'tag3']
@@ -18,30 +18,17 @@ class Counter extends Component {  //type cc
     render() { 
         return (
         <React.Fragment>
-            <span style = { {fontSize:15} } className={ this.getBadgeClasses() }>{this.formatCount()}</span>
-            <button style={{fontSize: 20}} className='btn btn-secondary btn-sm'>Increment</button>
             <ul>
                 { this.state.tags.map(tag => <li key={ tag } style={ { fontSize: 15, color: "black", margin:10}}> {tag} </li> ) }
-                
             </ul>
         </React.Fragment>
         );
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount(){
-        const { count } = this.state; //add the curly braces.
-        return count === 0 ? "Zero" : count;
-    }
 }
 
 //for classname, use Bootstrap
 //https://getbootstrap.com/docs/4.1/getting-started/introduction/
 //use classes for maintability 
  
-export default Counter;  //we are exporting
+export default Counter2;  //we are exporting
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+//import Counter from './components/counter';
+import Counter2 from './components/counter2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>  
-    <Counter />
+    <Counter2 />
   </React.StrictMode>
 );
 
