fdf19c9800ba871ac7ddd0ed1c425cf50b26442c
FrankBerrocal <f.berrocalazofeif887@mybvc.ca>
2023-10-20 16:55:29 -0700

    Conditional Rendering

--- counter-app/src/components/counter2.jsx
+++ counter-app/src/components/counter2.jsx
@@ -4,31 +4,34 @@ import React, { Component } from 'react';  //type imrc
 class Counter2 extends Component {  //type cc
     state ={ 
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
-
-
+        tags: ['tag1']
     };
 
-    //we define stayle using properties, then call the attribute in the html. also, inline as in the example.
-    styles = {
-        fontSize: 15,
-        fontWeight: 'bold'
+    //we dont have if else in JSX, create a method in JS and created them.
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>"There are no tags"</p>;
+
+        return <ul> { this.state.tags.map(tag => <li key={ tag } > {tag} </li> ) } </ul>;
     }
 
+    //render a message based on a single condition
     render() { 
         return (
         <React.Fragment>
-            <ul>
-                { this.state.tags.map(tag => <li key={ tag } style={ { fontSize: 15, color: "black", margin:10}}> {tag} </li> ) }
-            </ul>
+            { this.state.tags.length === 0 && 'Please create a new tag' }
+            {this.renderTags()}
         </React.Fragment>
         );
     }
 
 }
 
-//for classname, use Bootstrap
-//https://getbootstrap.com/docs/4.1/getting-started/introduction/
-//use classes for maintability 
+export default Counter2;  
+
+/*for classname, use Bootstrap
+
+https://getbootstrap.com/docs/4.1/getting-started/introduction/
+
  
-export default Counter2;  //we are exporting
+*/
+
